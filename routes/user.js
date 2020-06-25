a5b0a891a8b2a5bc0f8ab8efbc435b688b69f8b6
ghpark <ghpark@daou.co.kr>
2020-06-25 11:39:22 +0900

    init

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const result = require('../view/result');
 //mock데이터를 세팅합니다.
 mockTest.setApi();
 
-//http://locahost:5000/usr/search/mark 같은 url로 호출
+//http://locahost:5000/user/search/mark 같은 url로 호출
 router.get('/search/:username',function(req,res) {
     result.userInfo(req,res);
 });
